7ceb33f7b1f6031ffdcd1b19016921c968d3088c
Carola Heinzel <164475282+CarolaHeinzel@users.noreply.github.com>
2025-05-19 11:47:30 +0200

    Create filtered.sh

---
+++ Select_SNPs/filtered.sh
@@ -0,0 +1,6 @@
+for chr in {1..22}; do
+  file="EUR_only_ALL.chr${chr}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz.recode.vcf"
+  vcftools --vcf ${file} --thin 2000 --min-alleles 2 --max-alleles 2 --non-ref-ac 2 --recode --out filtered_${file}
+  vcftools --vcf filtered_${file}.recode.vcf --maf 0.05 --recode --out maf_filtered_chr${chr}
+done
+bcftools concat -O z -o combined_filtered.vcf.gz maf_filtered_chr{1..22}.recode.vcf
